efae70113d00618b598c311ae58fd762b4dfb3d1
Thiago Valls <olavalls@gmail.com>
2020-03-19 19:33:01 -0300

    Creating session class

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,10 @@
 <?php
 
+use src\Core\Session;
 use src\Models\User;
 
 require(__DIR__ . "/src/Support/Config.php");
 require(__DIR__ . "/src/autoload.php");
 
-    // init application here
-    $model = new User;
-    
-    $user = $model->load(44);
-
-    if($user) {
-        var_dump($user);
-    }
-    
+   
     
--- src/Core/Session.php
+++ src/Core/Session.php
@@ -1,7 +1,62 @@
 <?php
 
-    namespace src\Core;
+namespace src\Core;
 
-    class Session {
-
+class Session
+{
+    public function __construct()
+    {
+        if (!session_id()) {
+            session_save_path(CONF_SES_PATH);
+            session_start();
+        }
     }
+
+    public function __get($name)
+    {
+        if (!empty($_SESSION[$name])) {
+            return $_SESSION[$name];
+        }
+
+        return null;
+    }
+
+    public function __isset($name): bool
+    {
+        return $this->has($name);
+    }
+
+    public function all(): ?object
+    {
+        return (object)$_SESSION;
+    }
+
+    public function set(string $key, $value): Session
+    {
+        $_SESSION[$key] = (is_array($value) ? (object) $value : $value);
+        return $this;
+    }
+
+    public function unset(string $key): Session
+    {
+        unset($_SESSION[$key]);
+        return $this;
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public function regenerate(): Session
+    {
+        session_regenerate_id(true);
+        return $this;
+    }
+
+    public function destroy()
+    {
+        session_destroy();
+        return $this;
+    }
+}
