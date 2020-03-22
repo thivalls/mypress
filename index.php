e2b2829f933095c23e28d9733a5c24f650ae1cdd
valls <thiago@agenciatw.com.br>
2020-03-22 20:36:14 -0300

    Creating crop image service

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
 <?php
 
-
 require_once(__DIR__ . '/vendor/autoload.php');
-
-use src\Support\Upload;
-
-$upload = new Upload();
-
-$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
-if($post && $_POST['send'] == "image") {
-    $u = $upload->image($_FILES['file'], $post['name'] ?? $_FILES['name']);
-    // // $u = $upload->image($_FILES['file'], $post['name']);
-
-    if(!empty($u)) {
-        var_dump($u);
-    }else {
-        echo $upload->message();
-    }
-}
-
-$formSend = 'image';
-
-?>
-<form action="./" name="upload" method="post" enctype="multipart/form-data">
-    <input name="send" value="<?= ($formSend ?? ""); ?>"/>
-    <input name="name" type="text" value="Nome do arquivo"/>
-    <input name="file" type="file" required/>
-    <button class="green"><?= $formSend; ?></button>
-</form>
--- src/Support/Thumb.php
+++ src/Support/Thumb.php
@@ -1,37 +1,62 @@
 <?php
 
-    namespace src\Support;
+namespace src\Support;
 
-    use CoffeeCode\Cropper\Cropper;
+use CoffeeCode\Cropper\Cropper;
 
-    class Thumb {
-        private $cropper;
-        private $uploads;
+/**
+ * FSPHP | Class Thumb
+ *
+ * @author Robson V. Leite <cursos@upinside.com.br>
+ * @package Source\Support
+ */
+class Thumb
+{
+    /** @var Cropper */
+    private $cropper;
 
-        public function __construct()
-        {
-            $this->cropper = new Cropper(CONF_IMAGE_CACHE, CONF_IMAGE_QUALITY["jpg"], CONF_IMAGE_QUALITY["png"]);
-            $this->uploads = CONF_UPLOAD_DIR;
-        }
+    /** @var string */
+    private $uploads;
 
-        public function make(string $image, int $width, int $height = null)
-        {
-            $this->cropper->make("{$this->uploads}/{$image}", $width, $height);
-        }
+    /**
+     * Thumb constructor.
+     */
+    public function __construct()
+    {
+        $this->cropper = new Cropper(CONF_IMAGE_CACHE, CONF_IMAGE_QUALITY['jpg'], CONF_IMAGE_QUALITY['png']);
+        $this->uploads = CONF_UPLOAD_DIR;
+    }
 
-        public function flush(string $image = null): void
-        {
-            if($image) {
-                $this->cropper->flush("{$this->uploads}/{$image}");
-                return;
-            }
+    /**
+     * @param string $image
+     * @param int $width
+     * @param int|null $height
+     * @return string
+     */
+    public function make(string $image, int $width, int $height = null): string
+    {
+        return $this->cropper->make("{$this->uploads}/{$image}", $width, $height);
+    }
 
-            $this->cropper->flush();
+    /**
+     * @param string|null $image
+     */
+    public function flush(string $image = null): void
+    {
+        if ($image) {
+            $this->cropper->flush("{$this->uploads}/{$image}");
             return;
         }
 
-        public function cropper(): Cropper
-        {
-            return $this->cropper;
-        }
+        $this->cropper->flush();
+        return;
     }
+
+    /**
+     * @return Cropper
+     */
+    public function cropper(): Cropper
+    {
+        return $this->cropper;
+    }
+}
